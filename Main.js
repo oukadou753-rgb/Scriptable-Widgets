4f72e884e82158f4683984ec9dd24abdbd4353cd
oukadou753-rgb <oukadou753@gmail.com>
2026-03-06 09:17:52 +0900

    Main.js を更新

--- Main.js
+++ Main.js
@@ -1,3 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: magic;
 const DEFAULT_APP_ID = "Weather"
 
 const APP_DEV_MODE = false
