ebce262e4a9570a5d747adcda6a91b91e3a82ea4
oukadou753-rgb <oukadou753@gmail.com>
2026-03-06 13:26:04 +0900

    Main.js を更新

--- Main.js
+++ Main.js
@@ -1,6 +1,9 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: blue; icon-glyph: magic;
+// icon-color: blue; icon-glyph: download;
+/**
+ * Main
+ **/
 const DEFAULT_APP_ID = "Weather"
 
 const APP_DEV_MODE = false
@@ -8,25 +11,41 @@ const APP_ID = args.widgetParameter || DEFAULT_APP_ID
 const APP_VERSION = "1.0.0"
 const APP_INFO = {id: APP_ID, version: APP_VERSION}
 
-function loadAppConfig(appId) {
-  try {
-    return importModule(`App_${appId}Config`)
-  } catch (e) {
-    console.error(`App config not found: ${appId}`)
-    return importModule(`App_${DEFAULT_APP_ID}Config`)
+const Main = {
+
+  loadAppConfig(appId) {
+    try {
+      return importModule(`App_${appId}Config`)
+    } catch (e) {
+      console.error(`App config not found: ${appId}`)
+      return importModule(`App_${DEFAULT_APP_ID}Config`)
+    }
+  },
+
+  async start(APP_CONFIG) {
+    if (APP_DEV_MODE || (config.runsInWidget && !config.runsInApp)) {
+      const WF_WidgetCore = importModule("WidgetFramework/WF_WidgetCore")
+      await (new WF_WidgetCore(APP_INFO, APP_CONFIG)).start();
+    } else {
+      const WF_AppCore = importModule("WidgetFramework/WF_AppCore");
+      (async() => {
+        await (new WF_AppCore(APP_INFO, APP_CONFIG)).start()
+      })()
+    }
+  },
+
+  async run() {
+    const APP_CONFIG = Main.loadAppConfig(APP_ID);
+    (async() => {
+      await Main.start(APP_CONFIG)
+    })()
   }
 }
+module.exports = Main;
 
-const APP_CONFIG = loadAppConfig(APP_ID)
-
-async function main() {
-  if (APP_DEV_MODE || (config.runsInWidget && !config.runsInApp)) {
-    const WF_WidgetCore = importModule("WF_WidgetCore")
-    await (new WF_WidgetCore(APP_INFO, APP_CONFIG)).start()
-  } else {
-    const WF_AppCore = importModule("WF_AppCore")
-    await (new WF_AppCore(APP_INFO, APP_CONFIG)).start()
-  }
+const module_name = module.filename.match(/[^\/]+$/ )[ 0 ].replace('.js', '');
+if (module_name == Script.name()) {
+  (async() => {
+    await Main.run()
+  })()
 }
-
-await main()
