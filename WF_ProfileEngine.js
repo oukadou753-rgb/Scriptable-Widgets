32ebc419344c901ea59a29055f22bea54b9c7a35
oukadou753-rgb <oukadou753@gmail.com>
2026-03-06 09:23:43 +0900

    WF_ProfileEngine.js を更新

--- WF_ProfileEngine.js
+++ WF_ProfileEngine.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: brown; icon-glyph: magic;
+// icon-color: deep-gray; icon-glyph: magic;
 module.exports = class WF_ProfileEngine {
 
   constructor(storage, defaultConfig) {
