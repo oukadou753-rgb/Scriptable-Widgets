8860354cc5cf1268070d2c33df0c4a90d84f9aa9
oukadou753-rgb <oukadou753@gmail.com>
2026-03-06 15:48:49 +0900

    WF_StorageEngine.js を更新

--- WidgetFramework/WF_StorageEngine.js
+++ WidgetFramework/WF_StorageEngine.js
@@ -1,6 +1,9 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: magic;
+/**
+ * WF_StorageEngine
+ **/
 const WF_DEV_MODE = true
 
 module.exports = class WF_StorageEngine {
