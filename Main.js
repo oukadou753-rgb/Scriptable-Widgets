7886875088d9ece3b908696635dcc874094a3ecd
oukadou753-rgb <oukadou753@gmail.com>
2026-03-21 16:57:07 +0900

    Main.js を更新

--- Main.js
+++ Main.js
@@ -2,141 +2,137 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: blue; icon-glyph: download;
 /**
- * Main
+ * DevWidget
  * UTF-8 日本語コメント
- **/
-const APP_DEV_MODE = true
-
+ * 2026/03/21 09:00
+ */
 const DEFAULT_APP_ID = "Weather"
+const DEFAULT_STRAGE_TYPE = "local"
 
+const APP_DEV_MODE = true
 const APP_ID = args.widgetParameter || DEFAULT_APP_ID
 const APP_VERSION = "1.0.0"
+const APP_CONFIG = `App_${APP_ID}Config`
 
-const DEFAULT_STRAGE_TYPE = "local" // "icloud", "local", "bookmark"
 const WF_MODULE_DIR = "WidgetFramework/"
 const WF_REPO = "oukadou753-rgb/scriptable-widget-framework"
 
 const APP_INFO = {
   debug: APP_DEV_MODE,
-
   id: APP_ID,
   version: APP_VERSION,
-
   storageType: DEFAULT_STRAGE_TYPE,
   moduleDir: WF_MODULE_DIR,
-  moduleCache: {},
-
-  frameworkRepo: WF_REPO
+  frameworkRepo: WF_REPO,
 }
 
-const ModuleLoader = importModule("ModuleLoader")
+const MODULES = {
+  WF_StorageEngine: { type: "both", path: WF_MODULE_DIR },
+  WF_WidgetRenderer: { type: "both", path: WF_MODULE_DIR },
+  WF_ProfileEngine: { type: "both", path: WF_MODULE_DIR },
+  WF_DataProvider: { type: "both", path: WF_MODULE_DIR },
+  WF_NotificationManager: { type: "both", path: WF_MODULE_DIR },
+  WF_CoreBase: { type: "both", path: WF_MODULE_DIR },
 
-// =========================
-// Export
-// =========================
-const Main = {
+  WF_MenuEngine: { type: "app", path: WF_MODULE_DIR },
+  WF_ConfigUI: { type: "app", path: WF_MODULE_DIR },
+  WF_NotificationHandlers: { type: "app", path: WF_MODULE_DIR },
+  WF_NotificationUI: { type: "app", path: WF_MODULE_DIR },
 
-  // =========================
-  // loadAppConfig
-  // =========================
-  loadAppConfig(appInfo) {
+  WF_AppCore: { type: "app", path: WF_MODULE_DIR },
+  WF_WidgetCore: { type: "widget", path: WF_MODULE_DIR },
 
-    const appId = appInfo.id
-    const moduleDir = appInfo.moduleDir
-    const moduleLoader = appInfo.moduleLoader
+  [APP_CONFIG]: { type: "both", path: "" },
+}
 
-    try {
-
-      return importModule(`App_${appId}Config`)
-
-    } catch (e) {
-
-      console.error(`App config not found: ${appId}`)
-      return moduleLoader.load(`${moduleDir}App_${appId}Config`)
-
-    }
-  },
-
+module.exports = {
+  
   // =========================
   // start
   // =========================
   async start(appInfo = APP_INFO) {
 
-    const appId = appInfo.id
-    const storageType = appInfo.storageType
-    const moduleDir = appInfo.moduleDir
-
-    const moduleLoader = new ModuleLoader(storageType)
-    appInfo.moduleLoader = moduleLoader
-
-    const appConfig = Main.loadAppConfig(appInfo)
-
     try {
 
-      const WF_StorageEngine = moduleLoader.load(`${moduleDir}WF_StorageEngine`)
-      const WF_WidgetRenderer = moduleLoader.load(`${moduleDir}WF_WidgetRenderer`)
-      const WF_ProfileEngine = moduleLoader.load(`${moduleDir}WF_ProfileEngine`)
-      const WF_DataProvider = moduleLoader.load(`${moduleDir}WF_DataProvider`)
-      const WF_NotificationManager = moduleLoader.load(`${moduleDir}WF_NotificationManager`)
-      const WF_CoreBase = moduleLoader.load(`${moduleDir}WF_CoreBase`)
+      await this.init(appInfo)
 
-      if (config.runsInWidget && !config.runsInApp) {
-
-        const WF_WidgetCore = moduleLoader.load(`${moduleDir}WF_WidgetCore`)
-
-        appInfo.moduleCache = {
-          ModuleLoader,
-          moduleLoader,
-
-          WF_StorageEngine,
-          WF_WidgetRenderer,
-          WF_ProfileEngine,
-          WF_DataProvider,
-          WF_NotificationManager,
-          WF_CoreBase
-        }
-
-        const widgetCore = new WF_WidgetCore(appInfo, appConfig, appInfo.moduleCache)
-        await widgetCore.start();
-
-      } else {
-
-        const WF_MenuEngine = moduleLoader.load(`${moduleDir}WF_MenuEngine`)
-        const WF_ConfigUI = moduleLoader.load(`${moduleDir}WF_ConfigUI`)
-        const WF_NotificationUI = moduleLoader.load(`${moduleDir}WF_NotificationUI`)
-
-        const WF_AppCore = moduleLoader.load(`${moduleDir}WF_AppCore`)
-
-        appInfo.moduleCache = {
-          ModuleLoader,
-          moduleLoader,
-
-          WF_StorageEngine,
-          WF_WidgetRenderer,
-          WF_MenuEngine,
-          WF_ProfileEngine,
-          WF_ConfigUI,
-          WF_DataProvider,
-          WF_NotificationManager,
-          WF_NotificationUI,
-          WF_CoreBase
-        }
-
-        const appCore = new WF_AppCore(appInfo, appConfig, appInfo.moduleCache)
-        await appCore.start()
-
-      }
-      
     } catch(e) {
 
-      console.error("Error: " + e.message)
-      this.errorWidget(e)
+      this.catch(e)
 
     } finally {
 
-      Script.complete()
+      this.terminate(appInfo)
 
     }
+
+  },
+
+  // =========================
+  // init
+  // =========================
+  async init(appInfo) {
+
+      const ModuleLoader = importModule("ModuleLoader")
+      const moduleLoader = new ModuleLoader(appInfo.storageType)
+
+      let obj = {}
+
+      for (key in MODULES) {
+
+        const value = MODULES[key]
+        const path = value.path + key
+        const type = value.type
+
+        if (this.isImport(type)) {
+          try {
+            obj[key] = moduleLoader.load(path)
+          } catch(e) {
+            obj[key] = importModule(key)
+          }
+        }
+
+      }
+
+      if (config.runsInWidget) {
+        const widgetCore = new obj.WF_WidgetCore(appInfo, obj[APP_CONFIG], obj)
+        await widgetCore.start()
+      } else {
+        const appCore = new obj.WF_AppCore(appInfo, obj[APP_CONFIG], obj)
+        await appCore.start()
+      }
+
+  },
+
+  // =========================
+  // catch
+  // =========================
+  catch(e) {
+
+    console.error(e.message)
+    errorWidget(e)
+
+  },
+
+  // =========================
+  // terminate
+  // =========================
+  terminate(appInfo) {
+
+    Script.complete()
+
+  },
+
+  // =========================
+  // isImport
+  // =========================
+  isImport(type) {
+    if (config.runsInWidget) {
+      if(type == "widget" || type == "both") return true
+    } else {
+      if(type == "app" || type == "both") return true
+    }
+    return false
   },
 
   // =========================
@@ -146,56 +142,58 @@ const Main = {
 
     APP_INFO[key] = values
 
-  },
-
-  // =========================
-  // errorWidget
-  // =========================
-  errorWidget(e) {
-
-    const color = [ Color.yellow(), Color.black(), Color.red() ]
-
-    let w = new ListWidget()
-    w.setPadding(10, 10, 10, 10)
-    w.backgroundColor = color[0]
-
-    this._addText(w, "WidgetFramework", Font.semiboldSystemFont(13), color[1], "left")
-    w.addSpacer()
-    this._addText(w, "Script Stopped", Font.boldSystemFont(18), color[1], "center")
-    w.addSpacer(10)
-    this._addText(w, e.message, Font.boldSystemFont(16), color[2], "center")
-    w.addSpacer()
-    this._addText(w, APP_INFO.storageType + " " + new Date().toLocaleTimeString(), Font.semiboldSystemFont(11), color[1], "right")
-
-    if (config.runsInWidget) Script.setWidget(w)
-    else w.presentLarge()
-  },
-
-  // =========================
-  // _addText
-  // =========================
-  _addText(w, text, font, color, aling) {
-
-    let t = w.addText(text)
-    if (font) t.font = font
-    if (color) t.textColor = color
-    if (aling == "right") t.rightAlignText()
-    else if (aling == "center") t.centerAlignText()
   }
 
 }
 
-module.exports = Main
+// =========================
+// errorWidget
+// =========================
+function errorWidget(e) {
 
+  const color = [ Color.black(), Color.red() ]
+  const bgColor = Color.yellow()
+
+  let w = new ListWidget()
+  w.setPadding(10, 10, 10, 10)
+  w.backgroundColor = bgColor
+
+  addText(w, "WidgetFramework", Font.semiboldSystemFont(13), color[0], "left")
+  w.addSpacer()
+  addText(w, "Script Stopped", Font.boldSystemFont(18), color[0], "center")
+  w.addSpacer(10)
+  addText(w, e.message, Font.boldSystemFont(16), color[1], "center")
+  if (e.stack) {
+    w.addSpacer(6)
+    addText(w, e.stack.slice(0, 120), Font.systemFont(10), color[1], "left")
+  }
+  w.addSpacer()
+  addText(w, APP_INFO.storageType + " " + new Date().toLocaleTimeString(), Font.semiboldSystemFont(11), color[1], "right")
+
+  if (config.runsInWidget) Script.setWidget(w)
+  else w.presentLarge()
+
+}
+
+// =========================
+// addText
+// =========================
+function addText(w, text, font, color, aling) {
+
+  let t = w.addText(text)
+  if (font) t.font = font
+  if (color) t.textColor = color
+  if (aling == "right") t.rightAlignText()
+  else if (aling == "center") t.centerAlignText()
+
+}
+  
 // =========================
 // Module Test
 // =========================
 const module_name = module.filename.match(/[^\/]+$/ )[ 0 ].replace('.js', '');
 if (module_name == Script.name()) {
   (async() => {
-    const appInfo = APP_INFO
-    
-    console.log("USING: " + appInfo.storageType)
-    await Main.start(appInfo)
+    await module.exports.start()
   })()
 }
